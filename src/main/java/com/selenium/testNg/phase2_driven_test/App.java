2e16c0fcb40e58a89998068aefc3f0e6e2d02aaf
malavika <malavika.rathnakar@gmail.com>
2023-12-20 16:52:12 +0530

    added files

---
+++ src/main/java/com/selenium/testNg/phase2_driven_test/App.java
@@ -0,0 +1,13 @@
+package com.selenium.testNg.phase2_driven_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/selenium/testNg/phase2_driven_test/AppTest.java
@@ -0,0 +1,20 @@
+package com.selenium.testNg.phase2_driven_test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
